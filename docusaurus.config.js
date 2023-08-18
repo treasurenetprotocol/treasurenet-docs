66de68680ea6395d614bda567a7f8010e9bced68
chriszhao1988 <chriszhao1988@hotmail.com>
2023-08-18 18:07:00 +0800

    update content for Silvia.Wu

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,6 +3,8 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const math = require('remark-math');
+const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -61,6 +63,8 @@ const config = {
                     showLastUpdateAuthor: true,
                     showLastUpdateTime: true,
                     sidebarCollapsible: true,
+                    remarkPlugins: [math],
+                    rehypePlugins: [katex],
                 },
                 blog: {
                     showReadingTime: false,
@@ -78,6 +82,15 @@ const config = {
                 },
             }),
         ],
+    ],
+    stylesheets: [
+        {
+            href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+            type: 'text/css',
+            integrity:
+                'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+            crossorigin: 'anonymous',
+        },
     ],
 
     themeConfig:
